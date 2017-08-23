0e2296fb761835a135ae2810d804d8f6cf6b954e
frontexas <forfron@gmail.com>
2017-08-23 08:57:06 -0500

    Added nav options and prof pic to set profile to nav bar

--- src/js/components/layout/Nav.js
+++ src/js/components/layout/Nav.js
@@ -10,40 +10,116 @@ export default class Nav extends React.Component{
 		this.props.signOut();
 	}
 
+	inNonAuthenticatedPages(pathname){
+		const non_authenticated_page = {"/":true,"/sign_in":true,"/sign_up":true}
+		return pathname in non_authenticated_page;
+	}
+
 	render(){
 		const {pathname} = this.props.location;
 		return(
-			<nav>
-				<div className="nav-wrapper">
-					<div class="brand-logo">
-						<Link to="/" class="nav-logo" href="./">
-							<span><img class="nav-tr-icon" src={require("../../imgs/icon.png")}></img></span>
-							<span class="trurecruit-text-logo white-text">Tru<b>Recruit</b></span>
-						</Link>
-					</div>
-					<a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
-					<ul className="right hide-on-med-and-down">
+			<div>
+				<ul id="dropdown" class="dropdown-content nav-dropdown-ul">
+					<li class="nav-dropdown-list disabled">
+						<div>
+							<span>Signed in as</span>
+							<br/>
+							<span style={{fontSize:20}}><b>Fahran Kamili</b></span>
+						</div>
+						
+					</li>
+					<li className="nav-dropdown-list">
+						<Link to="/profile_set_up" class="printshare-button hvr-shadow">
+                          <i class="fa fa-cog" style={{fontSize:20}}></i>
+                          <span style={{fontSize:15}}>Profile Set Up</span>
+                         </Link>
+					</li>
+					<li className="divider"></li>
+					<li className="nav-dropdown-list">
+						<div class="printshare-button hvr-shadow" onClick={this.handleSignOutClick.bind(this)}>
+                          	<span style={{fontSize:15}}>Sign Out</span>
+                         </div>
+					</li>
+				</ul>
+				<nav>
+					<div className="nav-wrapper">
+						<div class={this.inNonAuthenticatedPages(pathname) ? "brand-logo" : "brand-logo center"}>
+							{
+								this.inNonAuthenticatedPages(pathname) ? 
+									<Link to="/" class="nav-logo" href="./">
+										<span><img class="nav-tr-icon" src={require("../../imgs/icon.png")}></img></span>
+										<span class="trurecruit-text-logo white-text">Tru<b>Recruit</b></span>
+									</Link>
+								:
+									<div class="nav-logo" href="./">
+										<span><img class="nav-tr-icon" src={require("../../imgs/icon.png")}></img></span>
+										<span class="trurecruit-text-logo white-text">Tru<b>Recruit</b></span>
+									</div>
+							}
+
+						</div>
 						{
-							pathname == '/' || pathname == '/sign_in' || pathname == '/sign_up' ? 
-							(	
-								<div>
-									<li><Link class="waves-effect waves-light btn tr-green" to="sign_up">Sign Up</Link></li>	
-				        			<li><Link class="waves-effect waves-light btn tr-green mt10" to="sign_in">Sign In</Link></li>	
-								</div>
-				        	)
-							: 
-							(
-								<li><button class="waves-effect waves-light btn tr-green" onClick={this.handleSignOutClick.bind(this)} style={{marginRight:10}}>Sign Out</button></li>	
-							)
+							this.inNonAuthenticatedPages(pathname) ? 
+								<a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+							:
+								''
+
+						}
+						<ul className={this.inNonAuthenticatedPages(pathname) ? "right hide-on-med-and-down" : "right"}>
+							{
+								this.inNonAuthenticatedPages(pathname) ? 
+								(	
+									<div>
+										<li><Link class="waves-effect waves-light btn tr-green" to="sign_up">Sign Up</Link></li>	
+					        			<li><Link class="waves-effect waves-light btn tr-green mt10" to="sign_in">Sign In</Link></li>	
+									</div>
+					        	)
+								: 
+								(
+									<li>
+										<a href="#!" class="dropdown-button" data-activates="dropdown">
+											<img src="http://www.radfaces.com/images/avatars/alan-frog.jpg"
+												 class="profpic-nav"
+												 alt=""
+											/>
+										</a>
+									</li>	
+								)
+							}
+							
+						</ul>
+						{
+							!this.inNonAuthenticatedPages(pathname) ? 
+								<ul className="left" style={{marginLeft:35}}>
+									<li className={pathname == "/dashboard" ? "active":""} style={{position:'relative'}}>
+										{
+											pathname == '/dashboard' ?
+												<div className="nav-active-marker"></div>
+											:
+											''
+										}
+										<Link to="/dashboard">
+											<span style={{fontSize:20}}>Dashboard</span>
+										</Link>
+
+									</li>
+								</ul>
+							: ''
+						}
+
+						{
+							this.inNonAuthenticatedPages(pathname) ? 
+							<ul className="side-nav" id="mobile-demo">
+								<li><Link class="waves-effect waves-light btn tr-green" to="sign_up">Sign Up</Link></li>	
+					        	<li><Link class="waves-effect waves-light btn tr-green mt10" to="sign_in">Sign In</Link></li>
+							</ul>
+							:
+								''
 						}
 						
-					</ul>
-					<ul className="side-nav" id="mobile-demo">
-						<li><Link class="waves-effect waves-light btn tr-green" to="sign_up">Sign Up</Link></li>	
-				        <li><Link class="waves-effect waves-light btn tr-green mt10" to="sign_in">Sign In</Link></li>
-					</ul>
-				</div>
-			</nav>
+					</div>
+				</nav>
+			</div>
 		)
 	}
 }
