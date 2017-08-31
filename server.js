a22a33653a392e78847ed4bf18bd0ae4be38e016
frontexas <forfron@gmail.com>
2017-08-31 15:09:33 -0500

    Remove twilio credentials

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 var port = process.env.PORT || 8080;
--- src/api/controllers/twilioController.js
+++ src/api/controllers/twilioController.js
@@ -1,6 +1,6 @@
 const twilio = require('twilio');
-const account_sid = 'AC71f8c3c9fc9055bc12d6bab9a035b5b8';
-const auth_token = 'ffceae70948d39c9ea6c1f2bdb2c5478';
+const account_sid = process.env.TWILIO_ACOUNT_SID;
+const auth_token = process.env.TWILIO_AUTH_TOKEN;
 const client = new twilio(account_sid,auth_token);
 const _from = '+15123615127';
 
